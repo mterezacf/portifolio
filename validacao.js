75a64c3c471dec990119bdf8c03eb274a07fa36a
Maria Tereza C. Ferreira <95322597+mterezacf@users.noreply.github.com>
2023-06-11 12:48:33 -0300

    Trabalho para matéria de Padrões WEB - SENAC

---
+++ validacao.js
@@ -0,0 +1,68 @@
+function mordeMelancia() {
+    document.getElementById("melanciainteira").src="melanciacomida.jpg";
+
+}
+function melanciaInteira() {
+    document.getElementById("melanciainteira").src="melanciainteira.jpg";
+}
+
+function calcularIMC() {
+    var peso = parseFloat(document.getElementById("cPeso").value);
+    var altura = parseFloat(document.getElementById("cAltura").value);
+    imc = peso / (altura*altura);
+    
+    document.getElementById("cIMC").value = imc.toFixed(2);
+}
+    
+function calcularDieta() {
+    var nivelAtividade = document.getElementById("cAtividadeFisica").value;
+    var objetivo = document.getElementById("cObjetivo").value;
+    var peso = parseFloat(document.getElementById("cPeso").value);
+
+    var fatorAtividade = 0;
+      switch (nivelAtividade) {
+        case "sedentario":
+          fatorAtividade = 1.2;
+          break;
+        case "leve":
+          fatorAtividade = 1.375;
+          break;
+        case "moderado":
+          fatorAtividade = 1.55;
+          break;
+        case "ativo":
+          fatorAtividade = 1.725;
+          break;
+        case "muitoativo":
+          fatorAtividade = 1.9;
+          break;
+        default:
+          fatorAtividade = 1.2;
+          break;
+      }
+
+    var fatorObjetivo = 0;
+      switch (objetivo) {
+        case "perder":
+          fatorObjetivo = 20;
+          break;
+        case "manter":
+          fatorObjetivo = 25;
+          break;
+        default:
+          fatorObjetivo = 30;
+          break;
+      }
+
+    var necessidadeCaloricaDiaria = fatorAtividade * fatorObjetivo * peso;
+
+    document.getElementById("cDieta").value = necessidadeCaloricaDiaria.toFixed(0);
+    }
+      
+function validacao() {
+    window.alert("Formulario Enviado")
+  }
+
+function validacaofaleconosco() {
+    window.alert("Mensagem Enviada com Sucesso")
+  }
